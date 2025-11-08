9de7b6ad685b04ab77d8492b37a360dfa82f3cb4
Zachary Ware <zach@python.org>
2025-11-08 16:12:54 -0600

    Migrate extra_factory_args to worker configuration

--- master/custom/workers.py
+++ master/custom/workers.py
@@ -28,7 +28,7 @@ class CPythonWorker:
         not_branches=None,
         parallel_builders=None,
         parallel_tests=None,
-        exclude_test_resources=None,
+        extra_factory_args=None,
     ):
         self.name = name
         self.tags = tags or set()
@@ -36,7 +36,7 @@ class CPythonWorker:
         self.not_branches = not_branches
         self.parallel_builders = parallel_builders
         self.parallel_tests = parallel_tests
-        self.exclude_test_resources = exclude_test_resources
+        self.extra_factory_args = extra_factory_args or {}
         worker_settings = settings.workers[name]
         owner = name.split("-")[0]
         owner_settings = settings.owners[owner]
@@ -72,6 +72,10 @@ def get_workers(settings):
             not_branches=['3.10'],
             parallel_tests=2,
             parallel_builders=2,
+            extra_factory_args={
+                # Increase the timeout on this slow worker
+                "timeout_factor": 2,
+            },
         ),
         cpw(
             name="billenstein-macos",
@@ -135,6 +139,10 @@ def get_workers(settings):
             tags=['linux', 'unix', 'rhel', 'ppc64le'],
             parallel_tests=10,
             branches=['3.10', '3.11', '3.12'],
+            extra_factory_args={
+                # Increase the timeout on this slow worker
+                "timeout_factor": 2,
+            },
         ),
         cpw(
             name="cstratak-CentOS9-ppc64le",
@@ -285,7 +293,9 @@ def get_workers(settings):
             tags=['linux', 'unix', 'raspbian', 'debian', 'arm'],
             parallel_tests=2,
             # Problematic ISP causes issues connecting to testpython.net
-            exclude_test_resources=['urlfetch', 'network'],
+            extra_factory_args=dict(
+                exclude_test_resources=['urlfetch', 'network'],
+            ),
         ),
         cpw(
             name="rkm-arm64-ios-simulator",
@@ -317,7 +327,9 @@ def get_workers(settings):
             # Tests fail with latin1 encoding on 3.12, probably earlier
             not_branches=['3.12', '3.11', '3.10'],
             # Problematic ISP causes issues connecting to testpython.net
-            exclude_test_resources=['urlfetch', 'network'],
+            extra_factory_args=dict(
+                exclude_test_resources=['urlfetch', 'network'],
+            ),
         ),
         cpw(
             name="ware-alpine",
